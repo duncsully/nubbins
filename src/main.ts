9380c2291a42514bb31f93c43f105967083d3cb4
duncsully <duncsully@gmail.com>
2022-09-09 21:05:23 -0500

    doc: Vanilla example

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,48 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { ReactComponent } from './react/ReactComponent'
 import React from 'react'
+import { counter, doubled } from './counter'
 
 // TODO:
-// - Vanilla
 // - Vue
 // - Lit
 // - Svelte
 // - Angular?
 // - Wrap everything in updating border
 // - search and persisted examples
+
+// ============================================================================
+// Vanilla
+// ============================================================================
+const setupVanilla = () => {
+  const vanillaInput = document.querySelector(
+    '#vanilla-input'
+  ) as HTMLInputElement
+  const vanillaDoubled = document.querySelector('#vanilla-doubled')
+
+  vanillaInput.valueAsNumber = counter.get()
+
+  counter.subscribe(() => {
+    if (vanillaInput) {
+      vanillaInput.valueAsNumber = counter.get()
+    }
+  })
+
+  doubled.subscribe(() => {
+    if (vanillaDoubled) {
+      vanillaDoubled.textContent = doubled.get().toString()
+    }
+  })
+
+  vanillaInput?.addEventListener('change', e =>
+    counter.set((e?.target as HTMLInputElement).valueAsNumber)
+  )
+}
+setupVanilla()
+
+// ============================================================================
+// React
+// ============================================================================
 const reactContainer = document.querySelector('#react-app')
 // TODO: Figure out why type won't work
 // @ts-ignore
